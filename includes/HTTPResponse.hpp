48c410ea1b0e3719c25b352916cc67626de836e6
sthrace <71982194+sthrace@users.noreply.github.com>
2022-04-25 10:07:36 +0300

    Add files via upload

--- includes/HTTPResponse.hpp
+++ includes/HTTPResponse.hpp
@@ -1,42 +1,43 @@
-#ifndef HTTP_RESPONSE_HPP
-# define HTTP_RESPONSE_HPP
+#ifndef HTTPRESPONSE_HPP
+# define HTTPRESPONSE_HPP
 
-# include <fstream>
-# include <map>
+# include "Config.hpp"
 # include "HTTPRequest.hpp"
 # include "VirtualHost.hpp"
+# include "Location.hpp"
+# include "utils.hpp"
+# include <unistd.h>
+# include <fcntl.h>
 
 namespace ft {
-	class HTTPResponse {
-		public:
-			// HTTPResponse(HTTPRequest* httpReuest);
-			HTTPResponse(
-				HTTPRequest* httpReuest,
-				std::map<std::string,ft::VirtualHost>	&_virtualHosts
-			);
-			HTTPResponse(const HTTPResponse &other);
-			~HTTPResponse();
+    class HTTPResponse {
+    private:
+        HTTPRequest*							_req;
+        std::map<std::string,ft::VirtualHost>	&_vhost;
+        std::string								_resource;
+		std::string 							_res_path;
+		std::vector<std::string> 				_index;
+		bool 									_autoindex;
+        std::map<std::string, std::string>		_headers;
 
-			HTTPResponse &operator=(const HTTPResponse &other);
-			
-			const std::string&	getResponse();
-		protected:
-			HTTPRequest* 											_httpReuest;
-			std::map<std::string,ft::VirtualHost>			&_virtualHosts;
-			std::string												_response;
-
-			HTTPResponse();
-
-			void	run();
-			void	handleGetResponse();
-			void	handlePostResponse();
-			void	handleDeleteResponse();
-			void	setErrorResponse(int status, const std::string& description);
-
-			void	printConfigurations(); 	// DELETE THIS METHOD!
-	};
-	
-} // namespace ft
+//        HTTPResponse(const HTTPResponse &x);
+//        HTTPResponse &operator=(const HTTPResponse &x);
+		void 		init(void);
+		void 		getParams(const std::string &server_name);
+        std::string getMIME(std::string file);
+		bool validMethod(std::string &method);
+		std::string GetResponse(void);
+		std::string getError(size_t code);
+        std::string getStatus(size_t code);
+		std::string sendGet(void);
+		std::string sendPost(void);
+		std::string sendDelete(void);
+    public:
+        HTTPResponse(HTTPRequest* req, std::map<std::string,ft::VirtualHost> &vhost);
+        ~HTTPResponse(void);
+        std::string getResponse(void);
+    };
+}
 
 
-#endif
+# endif
